379943f2fd40106f22ecb6b19a2d66a3b95f5aa1
Kybldo <keyblademasterextra@gmail.com>
2025-01-10 16:50:49 +0100

    Buscaminas, buscado y minado

--- src/Tema3/Tema3Ejercicio15.java
+++ src/Tema3/Tema3Ejercicio15.java
@@ -11,11 +11,13 @@ public class Tema3Ejercicio15 {
 
         Scanner read = new Scanner(System.in);
 
-        Random roll = new Random();
-
         int[] mines = new int[20];
 
         String[] minesFilled = new String[20];
+
+        String[] board = new String[20];
+
+        Arrays.fill(board, "■");
 
         scrambleMines(mines);
 
@@ -25,7 +27,65 @@ public class Tema3Ejercicio15 {
 
         numToMines(minesFilled);
 
-        System.out.println(Arrays.toString(minesFilled));
+        String[] copyWinDisplay = Arrays.copyOf(minesFilled, 20);
+
+        int moves = 0;
+        boolean bomb = false;
+
+        System.out.println("¡Evita las minas!");
+
+        while (moves < 14 && !bomb){
+
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println(" ");
+            System.out.println("Introduce un número (1 - 20) para desvelar qué hay en esa posición:");
+            System.out.println(" ");
+            System.out.println(Arrays.toString(board));
+            System.out.println(" ");
+
+            int selection = 0;
+
+            while (selection == 0 || selection > 20){
+
+                selection  = read.nextInt();
+
+                if (selection == 0 || selection > 20){
+
+                    System.out.println("Introduce un número válido.");
+                    System.out.println(" ");
+
+                }
+
+            }
+
+            selection--;
+
+            if (check(minesFilled, board, selection)){
+
+                bomb = true;
+
+            } else {
+
+                uncheck(minesFilled, board, selection);
+                moves++;
+
+            }
+
+        }
+
+        System.out.println(" ");
+
+        if (bomb) {
+
+            System.out.println("¡Has pisado una mina!");
+
+        } else {
+
+            System.out.println(Arrays.toString(copyWinDisplay));
+            System.out.println(" ");
+            System.out.println("¡Has ganado!");
+
+        }
 
     }
 
@@ -73,7 +133,7 @@ public class Tema3Ejercicio15 {
 
             } else {
 
-                if (mines[i] == 7 || mines[i] == 8){
+                if (mines[i] == 7 || mines[i] == 8 || mines[i] == 9){
 
                     mines[i + 1]++;
                     mines[i - 1]++;
@@ -90,7 +150,7 @@ public class Tema3Ejercicio15 {
 
         for (int i = 0; i < minesFilled.length; i++){
 
-            int temp = 0;
+            int temp;
 
             temp = Integer.parseInt(String.valueOf(mines[i]));
 
@@ -104,13 +164,45 @@ public class Tema3Ejercicio15 {
 
         for (int i = 0; i < minesFilled.length; i++){
 
-            if (Objects.equals(minesFilled[i], "7") || Objects.equals(minesFilled[i], "8")){
+            if (Objects.equals(minesFilled[i], "7") || Objects.equals(minesFilled[i], "8") || Objects.equals(minesFilled[i], "9")){
 
                 minesFilled[i] = "*";
 
             }
 
         }
+
+    }
+
+    public static boolean check(String[] minesFilled, String[] board, int num) {
+
+        boolean bomb = false;
+
+        String tempPairs1 = minesFilled[num]; //a
+        String tempPairs2 = board[num]; //b
+
+        minesFilled[num] = board[num]; //a = 1
+        board[num] = tempPairs1; //1 = a
+
+        minesFilled[num] = board[num]; //b = 2
+        board[num] = tempPairs2; //2 = b
+
+        if (Objects.equals(minesFilled[num], "*")) {
+
+            bomb = true;
+
+        }
+
+        return bomb;
+
+    }
+
+    public static void uncheck(String[] minesFilled, String[] board, int num) {
+
+        String tempPairs1 = minesFilled[num]; //a
+
+        minesFilled[num] = board[num]; //a = 1
+        board[num] = tempPairs1; //1 = a
 
     }
 
