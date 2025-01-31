bbaf448f8df286d6c9a3195c811d03a922fd7a01
Kybldo <keyblademasterextra@gmail.com>
2025-01-31 13:37:41 +0100

    Banco terminado (pendiente revisar (me va a destruir)) -- ahora comentado

--- src/Tema4/PruebaCuentas.java
+++ src/Tema4/PruebaCuentas.java
@@ -10,22 +10,28 @@ public class PruebaCuentas {
 
         Scanner read = new Scanner(System.in);
 
+        // Crea array vacío de personas a registrar en el banco
+
         PersonaBanco[] persons = new PersonaBanco[10];
         int numberOfPersons = 0;
+
+        // Crea un segundo array que sirve para enseñar visualmente qué espacios del primer array tienen una persona
 
         String[] accountDisplay = new String[persons.length];
 
         Arrays.fill(accountDisplay, "*");
 
+        // A partir de aquí es el menú
+
         int menuChoice = 0;
 
         while (menuChoice == 0) {
 
-            if (persons[0] != null) {
+            //Si la variable numberOfPersons equivale a más de 0, se empezarán a registrar personas en el segundo array para demostración visual
 
-                accountDisplay[numberOfPersons - 1] = "■";
+            if (persons[0] != null) accountDisplay[numberOfPersons - 1] = "■";
 
-            }
+            // El texto del menú
 
             System.out.println(" ");
 
@@ -51,70 +57,76 @@ public class PruebaCuentas {
 
             switch (menuChoice) {
 
+                // Crear persona
+
                 case 1 -> {
 
                     createPerson(numberOfPersons, persons);
 
                     numberOfPersons++;
-
                     menuChoice = 0;
 
                 }
+
+                // Crear una cuenta y asociarla a una persona anteriormente creada
 
                 case 2 -> {
 
                     createAccount(persons, numberOfPersons);
-
                     menuChoice = 0;
 
                 }
+
+                // Mostrar información de una persona (sus cuentas y sus respectivos balances)
 
                 case 3 -> {
 
                     showInfo(persons, numberOfPersons);
-
                     menuChoice = 0;
 
                 }
+
+                // Ingresar la nómina a la cuenta de una persona (por defecto, 1200 euros)
 
                 case 4 -> {
 
                     monthlyCheck(persons, numberOfPersons);
-
                     menuChoice = 0;
 
                 }
+
+                // Enviar una cantidad personalizada de dinero a la cuenta de una persona
 
                 case 5 -> {
 
                     receiveMoney(persons, numberOfPersons);
-
                     menuChoice = 0;
 
                 }
+
+                // Transferir dinero de una cuenta a otra
 
                 case 6 -> {
 
                     transferMoney(persons, numberOfPersons);
-
                     menuChoice = 0;
 
                 }
+
+                // Detectar e imprimir morosos (personas que tengan cuentas con balances negativos)
 
                 case 7 -> {
 
                     printDefaulters(persons, numberOfPersons);
-
                     menuChoice = 0;
 
                 }
 
+                // Sale del programa
+
                 case 8 -> System.out.println("Has salido del programa.");
-
             }
-
         }
-
     }
 
     // Crear personas --------------------------------------------------------------------------------------------------
@@ -133,11 +145,10 @@ public class PruebaCuentas {
 
         person.setDni(dni);
 
-        if (numberOfPersons < persons.length) {
+        // Una vez introducido el DNI de la persona, se comprueba si se puede meter en el array; si se puede, se mete
+        // numberOfPersons se aumenta en el main tras llamar a la función
 
-            persons[numberOfPersons] = person;
-
-        }
+        if (numberOfPersons < persons.length) persons[numberOfPersons] = person;
     }
 
     // Crear cuentas ---------------------------------------------------------------------------------------------------
@@ -151,6 +162,9 @@ public class PruebaCuentas {
         System.out.println("Escribe el DNI de la persona:");
 
         String selectedPersonDNI = read.nextLine();
+
+        // Se crean las siguientes variables y se usa un for para recorrer el array de personas y comparar el DNI introducido con los guardados
+        // De coincidir los DNI, se cambia el número de la persona en la que operar por aquel que ha dado la equivalencia
 
         String personDNI;
         int personNumber = 0;
@@ -166,8 +180,9 @@ public class PruebaCuentas {
                 personNumber = i;
 
             }
-
         }
+
+        // Una vez confirmado el DNI, revisa el número de cuentas; si la persona ya tiene 3 cuentas registradas, no puede añadir más
 
         if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
@@ -187,6 +202,8 @@ public class PruebaCuentas {
 
                 int accountBalance = read.nextInt();
 
+                // Igual que al añadir las personas anteriormente, se hace lo mismo para las cuentas, siendo enviadas a su respectivo array
+
                 Cuenta account = new Cuenta();
 
                 account.setAccountNumber(accountNumber);
@@ -198,7 +215,6 @@ public class PruebaCuentas {
             }
 
         } else System.out.println("DNI inválido.");
-
     }
 
     // Ver información de la persona -----------------------------------------------------------------------------------
@@ -213,6 +229,8 @@ public class PruebaCuentas {
 
         String selectedPersonDNI = read.nextLine();
 
+        // Se crean las siguientes variables y se usa un for para recorrer el array de personas y comparar el DNI introducido con los guardados
+
         String personDNI;
         int personNumber = 0;
         String finalPersonDNI = "";
@@ -227,16 +245,15 @@ public class PruebaCuentas {
                 personNumber = i;
 
             }
-
         }
+
+        // Se imprime su información si el DNI introducido coincide con el registrado; de lo contrario, imprime el error
 
         if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
             System.out.println(Arrays.toString(persons[personNumber].getAccounts()));
 
         } else System.out.println("DNI inválido.");
-
-
     }
 
     // Ingresar nómina en una cuenta -----------------------------------------------------------------------------------
@@ -251,6 +268,8 @@ public class PruebaCuentas {
 
         String selectedPersonDNI = read.nextLine();
 
+        // Se crean las siguientes variables y se usa un for para recorrer el array de personas y comparar el DNI introducido con los guardados
+
         String personDNI;
         int personNumber = 0;
         String finalPersonDNI = "";
@@ -265,8 +284,9 @@ public class PruebaCuentas {
                 personNumber = i;
 
             }
-
         }
+
+        // Igual que con el DNI, se comprueba el número de cuenta
 
         if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
@@ -275,6 +295,8 @@ public class PruebaCuentas {
             System.out.println("Introduce el número de cuenta a ingresar:");
 
             String selectedAccount = read.nextLine();
+
+            // Se crean las siguientes variables y se usa un for para recorrer el array de cuentas y comparar el número de cuenta introducido con los guardados
 
             String personAccount;
             int accountNumber = 0;
@@ -294,8 +316,9 @@ public class PruebaCuentas {
                     accountNumber = i;
 
                 }
-
             }
+
+            // Si los números de cuenta coinciden, se suma la cantidad de dinero de la nómina al balance de la cuenta
 
             if (Objects.equals(selectedAccount, finalAccountNumber)) {
 
@@ -312,7 +335,6 @@ public class PruebaCuentas {
             } else System.out.println("Número de cuenta inválido.");
 
         } else System.out.println("DNI inválido.");
-
     }
 
     // Recibir un pago en una cuenta -----------------------------------------------------------------------------------
@@ -326,6 +348,8 @@ public class PruebaCuentas {
         System.out.println("Escribe el DNI de la persona:");
 
         String selectedPersonDNI = read.nextLine();
+
+        // Se crean las siguientes variables y se usa un for para recorrer el array de personas y comparar el DNI introducido con los guardados
 
         String personDNI;
         int personNumber = 0;
@@ -341,8 +365,9 @@ public class PruebaCuentas {
                 personNumber = i;
 
             }
-
         }
+
+        // Si los DNI coinciden, se pide y se comprueba el número de cuenta
 
         if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
@@ -370,8 +395,9 @@ public class PruebaCuentas {
                     accountNumber = i;
 
                 }
-
             }
+
+            // Si los números de cuenta coinciden, se pide una cantidad de dinero, y se suma al balance de la cuenta
 
             if (Objects.equals(selectedAccount, finalAccountNumber)) {
 
@@ -394,7 +420,6 @@ public class PruebaCuentas {
             } else System.out.println("Número de cuenta inválido.");
 
         } else System.out.println("DNI inválido.");
-
     }
 
     // Realizar transferencia entre cuentas
@@ -408,6 +433,8 @@ public class PruebaCuentas {
         System.out.println("Escribe el DNI de la persona que va a recibir el dinero:");
 
         String selectedPersonDNI = read.nextLine();
+
+        // Se pide el primer DNI, el de la persona a recibir dinero
 
         String personDNI;
         int personNumber = 0;
@@ -423,8 +450,9 @@ public class PruebaCuentas {
                 personNumber = i;
 
             }
-
         }
+
+        // Si el DNI coincide, se pide y se comprueba el número de cuenta
 
         if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
@@ -452,8 +480,9 @@ public class PruebaCuentas {
                     accountNumber = i;
 
                 }
-
             }
+
+            // Si el número de cuenta coincide, se pide el DNI de la segunda persona, la que va a enviar el dinero
 
             if (Objects.equals(selectedAccount, finalAccountNumber)) {
 
@@ -477,8 +506,9 @@ public class PruebaCuentas {
                         personNumber2 = i;
 
                     }
-
                 }
+
+                // Si coincide, igual que antes, se pide y se comprueba el número de cuenta del que sacar el dinero
 
                 if (Objects.equals(selectedPersonDNI2, finalPersonDNI2)) {
 
@@ -506,8 +536,9 @@ public class PruebaCuentas {
                             accountNumber2 = i;
 
                         }
-
                     }
+
+                    // Si coinciden, se pide la cantidad a sacar de la cuenta número 2, para ingresarla en la cuenta número 1
 
                     if (Objects.equals(selectedAccount2, finalAccountNumber2)) {
 
@@ -520,6 +551,8 @@ public class PruebaCuentas {
                         int accountBalance = accounts[accountNumber].getBalance();
 
                         int account2Balance = accounts2[accountNumber2].getBalance();
+
+                        // Se ingresa el dinero y se actualizan los valores para mostrarlos
 
                         accounts[accountNumber].setBalance(account2Balance - quantity);
 
@@ -542,7 +575,6 @@ public class PruebaCuentas {
             } else System.out.println("DNI inválido.");
 
         }
-
     }
 
     // Detectar e imprimir morosos
@@ -554,6 +586,9 @@ public class PruebaCuentas {
         boolean defaulter = false;
 
         int count = 0;
+
+        // Recorre con un for anidado el array de personas, y posteriormente sus respectivas cuentas
+        // Comprueba los balances de cada cuenta, y, de ser negativos, el DNI de la persona con la cuenta en negativo se imprime como moroso
 
         for (int i = 0; i < numberOfPersons; i++){
 
@@ -573,7 +608,6 @@ public class PruebaCuentas {
                     count++;
 
                 }
-
             }
 
             if (defaulter){
@@ -583,9 +617,6 @@ public class PruebaCuentas {
                 defaulter = false;
 
             }
-
         }
-
     }
-
 }
