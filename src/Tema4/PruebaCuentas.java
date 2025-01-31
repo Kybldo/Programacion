c49173284122e779f1105e59fe4215450690724d
Kybldo <keyblademasterextra@gmail.com>
2025-01-31 13:11:37 +0100

    Banco terminado (pendiente revisar (me va a destruir))

--- src/Tema4/PruebaCuentas.java
+++ src/Tema4/PruebaCuentas.java
@@ -19,9 +19,9 @@ public class PruebaCuentas {
 
         int menuChoice = 0;
 
-        while (menuChoice == 0){
+        while (menuChoice == 0) {
 
-            if (persons[0] != null){
+            if (persons[0] != null) {
 
                 accountDisplay[numberOfPersons - 1] = "■";
 
@@ -43,10 +43,13 @@ public class PruebaCuentas {
             System.out.println("3. Mostrar datos de una persona");
             System.out.println("4. Ingresar nómina a una persona");
             System.out.println("5. Enviar dinero a una cuenta");
+            System.out.println("6. Realizar una transferencia entre cuentas");
+            System.out.println("7. Detectar e imprimir morosos");
+            System.out.println("8. Salir");
 
             menuChoice = read.nextInt();
 
-            switch (menuChoice){
+            switch (menuChoice) {
 
                 case 1 -> {
 
@@ -90,6 +93,24 @@ public class PruebaCuentas {
 
                 }
 
+                case 6 -> {
+
+                    transferMoney(persons, numberOfPersons);
+
+                    menuChoice = 0;
+
+                }
+
+                case 7 -> {
+
+                    printDefaulters(persons, numberOfPersons);
+
+                    menuChoice = 0;
+
+                }
+
+                case 8 -> System.out.println("Has salido del programa.");
+
             }
 
         }
@@ -98,7 +119,7 @@ public class PruebaCuentas {
 
     // Crear personas --------------------------------------------------------------------------------------------------
 
-    public static void createPerson(int numberOfPersons, PersonaBanco[] persons){
+    public static void createPerson(int numberOfPersons, PersonaBanco[] persons) {
 
         Scanner read = new Scanner(System.in);
 
@@ -112,7 +133,7 @@ public class PruebaCuentas {
 
         person.setDni(dni);
 
-        if (numberOfPersons < persons.length){
+        if (numberOfPersons < persons.length) {
 
             persons[numberOfPersons] = person;
 
@@ -121,7 +142,7 @@ public class PruebaCuentas {
 
     // Crear cuentas ---------------------------------------------------------------------------------------------------
 
-    public static void createAccount(PersonaBanco[] persons, int numberOfPersons){
+    public static void createAccount(PersonaBanco[] persons, int numberOfPersons) {
 
         Scanner read = new Scanner(System.in);
 
@@ -135,11 +156,11 @@ public class PruebaCuentas {
         int personNumber = 0;
         String finalPersonDNI = "";
 
-        for (int i = 0; i < numberOfPersons; i++){
+        for (int i = 0; i < numberOfPersons; i++) {
 
             personDNI = persons[i].getDni();
 
-            if (Objects.equals(selectedPersonDNI, personDNI)){
+            if (Objects.equals(selectedPersonDNI, personDNI)) {
 
                 finalPersonDNI = personDNI;
                 personNumber = i;
@@ -148,11 +169,11 @@ public class PruebaCuentas {
 
         }
 
-        if (Objects.equals(selectedPersonDNI, finalPersonDNI)){
+        if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
             Cuenta[] accountsArray = persons[personNumber].getAccounts();
 
-            if (accountsArray[2] != null){
+            if (accountsArray[2] != null) {
 
                 System.out.println("No puedes añadir más cuentas.");
 
@@ -182,7 +203,7 @@ public class PruebaCuentas {
 
     // Ver información de la persona -----------------------------------------------------------------------------------
 
-    public static void showInfo(PersonaBanco[] persons, int numberOfPersons){
+    public static void showInfo(PersonaBanco[] persons, int numberOfPersons) {
 
         Scanner read = new Scanner(System.in);
 
@@ -196,11 +217,11 @@ public class PruebaCuentas {
         int personNumber = 0;
         String finalPersonDNI = "";
 
-        for (int i = 0; i < numberOfPersons; i++){
+        for (int i = 0; i < numberOfPersons; i++) {
 
             personDNI = persons[i].getDni();
 
-            if (Objects.equals(selectedPersonDNI, personDNI)){
+            if (Objects.equals(selectedPersonDNI, personDNI)) {
 
                 finalPersonDNI = personDNI;
                 personNumber = i;
@@ -209,7 +230,7 @@ public class PruebaCuentas {
 
         }
 
-        if (Objects.equals(selectedPersonDNI, finalPersonDNI)){
+        if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
             System.out.println(Arrays.toString(persons[personNumber].getAccounts()));
 
@@ -220,7 +241,7 @@ public class PruebaCuentas {
 
     // Ingresar nómina en una cuenta -----------------------------------------------------------------------------------
 
-    public static void monthlyCheck(PersonaBanco[] persons, int numberOfPersons){
+    public static void monthlyCheck(PersonaBanco[] persons, int numberOfPersons) {
 
         Scanner read = new Scanner(System.in);
 
@@ -234,11 +255,11 @@ public class PruebaCuentas {
         int personNumber = 0;
         String finalPersonDNI = "";
 
-        for (int i = 0; i < numberOfPersons; i++){
+        for (int i = 0; i < numberOfPersons; i++) {
 
             personDNI = persons[i].getDni();
 
-            if (Objects.equals(selectedPersonDNI, personDNI)){
+            if (Objects.equals(selectedPersonDNI, personDNI)) {
 
                 finalPersonDNI = personDNI;
                 personNumber = i;
@@ -247,7 +268,7 @@ public class PruebaCuentas {
 
         }
 
-        if (Objects.equals(selectedPersonDNI, finalPersonDNI)){
+        if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
             System.out.println(" ");
 
@@ -263,11 +284,11 @@ public class PruebaCuentas {
 
             Cuenta[] accounts = persons[personNumber].getAccounts();
 
-            for (int i = 0; i < numberOfAccounts; i++){
+            for (int i = 0; i < numberOfAccounts; i++) {
 
                 personAccount = accounts[i].getAccountNumber();
 
-                if (Objects.equals(selectedAccount, personAccount)){
+                if (Objects.equals(selectedAccount, personAccount)) {
 
                     finalAccountNumber = personAccount;
                     accountNumber = i;
@@ -276,7 +297,7 @@ public class PruebaCuentas {
 
             }
 
-            if (Objects.equals(selectedAccount, finalAccountNumber)){
+            if (Objects.equals(selectedAccount, finalAccountNumber)) {
 
                 int accountBalance = accounts[accountNumber].getBalance();
 
@@ -296,7 +317,7 @@ public class PruebaCuentas {
 
     // Recibir un pago en una cuenta -----------------------------------------------------------------------------------
 
-    public static void receiveMoney(PersonaBanco[] persons, int numberOfPersons){
+    public static void receiveMoney(PersonaBanco[] persons, int numberOfPersons) {
 
         Scanner read = new Scanner(System.in);
 
@@ -310,11 +331,11 @@ public class PruebaCuentas {
         int personNumber = 0;
         String finalPersonDNI = "";
 
-        for (int i = 0; i < numberOfPersons; i++){
+        for (int i = 0; i < numberOfPersons; i++) {
 
             personDNI = persons[i].getDni();
 
-            if (Objects.equals(selectedPersonDNI, personDNI)){
+            if (Objects.equals(selectedPersonDNI, personDNI)) {
 
                 finalPersonDNI = personDNI;
                 personNumber = i;
@@ -323,7 +344,7 @@ public class PruebaCuentas {
 
         }
 
-        if (Objects.equals(selectedPersonDNI, finalPersonDNI)){
+        if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
 
             System.out.println(" ");
 
@@ -339,11 +360,11 @@ public class PruebaCuentas {
 
             Cuenta[] accounts = persons[personNumber].getAccounts();
 
-            for (int i = 0; i < numberOfAccounts; i++){
+            for (int i = 0; i < numberOfAccounts; i++) {
 
                 personAccount = accounts[i].getAccountNumber();
 
-                if (Objects.equals(selectedAccount, personAccount)){
+                if (Objects.equals(selectedAccount, personAccount)) {
 
                     finalAccountNumber = personAccount;
                     accountNumber = i;
@@ -352,7 +373,7 @@ public class PruebaCuentas {
 
             }
 
-            if (Objects.equals(selectedAccount, finalAccountNumber)){
+            if (Objects.equals(selectedAccount, finalAccountNumber)) {
 
                 System.out.println(" ");
 
@@ -373,6 +394,197 @@ public class PruebaCuentas {
             } else System.out.println("Número de cuenta inválido.");
 
         } else System.out.println("DNI inválido.");
+
+    }
+
+    // Realizar transferencia entre cuentas
+
+    public static void transferMoney(PersonaBanco[] persons, int numberOfPersons) {
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println(" ");
+
+        System.out.println("Escribe el DNI de la persona que va a recibir el dinero:");
+
+        String selectedPersonDNI = read.nextLine();
+
+        String personDNI;
+        int personNumber = 0;
+        String finalPersonDNI = "";
+
+        for (int i = 0; i < numberOfPersons; i++) {
+
+            personDNI = persons[i].getDni();
+
+            if (Objects.equals(selectedPersonDNI, personDNI)) {
+
+                finalPersonDNI = personDNI;
+                personNumber = i;
+
+            }
+
+        }
+
+        if (Objects.equals(selectedPersonDNI, finalPersonDNI)) {
+
+            System.out.println(" ");
+
+            System.out.println("Introduce el número de cuenta que va a recibir el dinero:");
+
+            String selectedAccount = read.nextLine();
+
+            String personAccount;
+            int accountNumber = 0;
+            String finalAccountNumber = "";
+
+            int numberOfAccounts = persons[personNumber].getNumberOfAccounts();
+
+            Cuenta[] accounts = persons[personNumber].getAccounts();
+
+            for (int i = 0; i < numberOfAccounts; i++) {
+
+                personAccount = accounts[i].getAccountNumber();
+
+                if (Objects.equals(selectedAccount, personAccount)) {
+
+                    finalAccountNumber = personAccount;
+                    accountNumber = i;
+
+                }
+
+            }
+
+            if (Objects.equals(selectedAccount, finalAccountNumber)) {
+
+                System.out.println(" ");
+
+                System.out.println("Escribe el DNI de la persona que va a enviar el dinero:");
+
+                String selectedPersonDNI2 = read.nextLine();
+
+                String personDNI2;
+                int personNumber2 = 0;
+                String finalPersonDNI2 = "";
+
+                for (int i = 0; i < numberOfPersons; i++) {
+
+                    personDNI2 = persons[i].getDni();
+
+                    if (Objects.equals(selectedPersonDNI2, personDNI2)) {
+
+                        finalPersonDNI2 = personDNI2;
+                        personNumber2 = i;
+
+                    }
+
+                }
+
+                if (Objects.equals(selectedPersonDNI2, finalPersonDNI2)) {
+
+                    System.out.println(" ");
+
+                    System.out.println("Introduce el número de cuenta desde la que se va a enviar el dinero:");
+
+                    String selectedAccount2 = read.nextLine();
+
+                    String personAccount2;
+                    int accountNumber2 = 0;
+                    String finalAccountNumber2 = "";
+
+                    int numberOfAccounts2 = persons[personNumber2].getNumberOfAccounts();
+
+                    Cuenta[] accounts2 = persons[personNumber2].getAccounts();
+
+                    for (int i = 0; i < numberOfAccounts2; i++) {
+
+                        personAccount2 = accounts2[i].getAccountNumber();
+
+                        if (Objects.equals(selectedAccount2, personAccount2)) {
+
+                            finalAccountNumber2 = personAccount2;
+                            accountNumber2 = i;
+
+                        }
+
+                    }
+
+                    if (Objects.equals(selectedAccount2, finalAccountNumber2)) {
+
+                        System.out.println(" ");
+
+                        System.out.println("Introduce la cantidad a transferir:");
+
+                        int quantity = read.nextInt();
+
+                        int accountBalance = accounts[accountNumber].getBalance();
+
+                        int account2Balance = accounts2[accountNumber2].getBalance();
+
+                        accounts[accountNumber].setBalance(account2Balance - quantity);
+
+                        accounts2[accountNumber2].setBalance(accountBalance + quantity);
+
+                        accountBalance = accounts[accountNumber].getBalance();
+
+                        account2Balance = accounts2[accountNumber2].getBalance();
+
+                        System.out.println("Se han ingresado " + quantity + "€");
+
+                        System.out.println("El saldo de la cuenta que ha recibido es: " + account2Balance + "€");
+
+                        System.out.println("El saldo de la cuenta que ha enviado es: " + accountBalance + "€");
+
+                    } else System.out.println("Número de cuenta inválido.");
+
+                } else System.out.println("DNI inválido.");
+
+            } else System.out.println("DNI inválido.");
+
+        }
+
+    }
+
+    // Detectar e imprimir morosos
+
+    public static void printDefaulters(PersonaBanco[] persons, int numberOfPersons){
+
+        System.out.println(" ");
+
+        boolean defaulter = false;
+
+        int count = 0;
+
+        for (int i = 0; i < numberOfPersons; i++){
+
+            Cuenta[] accounts = persons[i].getAccounts();
+
+            int numberOfAccounts = persons[i].getNumberOfAccounts();
+
+            String dni = persons[i].getDni();
+
+            for (int j = 0; j < numberOfAccounts; j++) {
+
+                int balance = accounts[j].getBalance();
+
+                if (balance < 0){
+
+                    defaulter = true;
+                    count++;
+
+                }
+
+            }
+
+            if (defaulter){
+
+                System.out.println("La persona con el DNI " + dni + " es un/a moroso/a.");
+
+                defaulter = false;
+
+            }
+
+        }
 
     }
 
